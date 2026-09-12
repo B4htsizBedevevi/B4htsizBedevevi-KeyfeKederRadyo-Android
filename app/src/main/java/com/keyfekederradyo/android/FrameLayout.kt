d4e611db1ced4918a370e5382f520ad609fa247a
BahtsizBedevi <55953865+B4htsizBedevevi@users.noreply.github.com>
2026-09-12 16:42:04 +0300

    cleanup: use Android FrameLayout directly

--- app/src/main/java/com/keyfekederradyo/android/FrameLayout.kt
+++
@@ -1,9 +0,0 @@
-package com.keyfekederradyo.android
-
-import android.content.Context
-
-class FrameLayout(context: Context) : android.widget.FrameLayout(context) {
-    var gravity: Int = android.view.Gravity.NO_GRAVITY
-
-    class LayoutParams(width: Int, height: Int) : android.widget.FrameLayout.LayoutParams(width, height)
-}
